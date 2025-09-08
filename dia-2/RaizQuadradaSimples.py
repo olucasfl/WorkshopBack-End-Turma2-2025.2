cace33c460d8f187b64a63e3ddf55118ff2511db
lucasfariasleandro <lucasfariasleandro@gmail.com>
2025-09-08 15:24:11 -0300

    Revert "Tratamentos de error"
    This reverts commit 6888387131348acf4a438976ffadbcafb790bd5f.

--- dia-2/RaizQuadradaSimples.py
+++ dia-2/RaizQuadradaSimples.py
@@ -1,6 +1,6 @@
 import math;
 
-numero = int(input("Digite um número: "));
+numero = float(input("Digite um número: "));
 
 raiz = math.sqrt(numero);
 
--- dia-3/simpleTreatment.py
+++
@@ -1,2 +0,0 @@
-print("Olá, mundo!"
-# Ocorreu um erro pela falta de parênteses no print, mostrando um SyntaxError para que o código nunca foi fechado.
