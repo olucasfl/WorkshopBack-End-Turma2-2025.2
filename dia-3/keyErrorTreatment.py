e21b8ceaa6cff081de75db6f1e46c36fa1182038
lucasfariasleandro <lucasfariasleandro@gmail.com>
2025-09-08 15:31:51 -0300

    atualização error

--- dia-3/keyErrorTreatment.py
+++ dia-3/keyErrorTreatment.py
@@ -10,5 +10,7 @@ def key_error_treatment():
         print(f"O valor da chave '{chave}' é: {dados[chave]}")
     except KeyError:
         print(f"A chave '{chave}' não existe no dicionário.")
+    except Exception as e:
+        print(f"Ocorreu um erro inesperado: {e}")
 
 key_error_treatment()
