8b841bca1fcbdb834041f9fe176bea96b40bb9fd
lucasfariasleandro <lucasfariasleandro@gmail.com>
2025-09-09 14:28:07 -0300

    Removendo arquivos do projeto

--- myProject/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =  [
-    path('admin/', admin.site.urls),
-    path('app/', include('app.urls')),
-    path('', views.home, name='home'),
-]
